46e87d5b17a113651fdc3402e0ae01602059e616
OleksiiTaran <alexey4ik007@gmail.com>
2024-03-13 20:27:39 +0200

    fix for playwright config for saving json report

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['json', { fileName: "report.json" }]
+    ['json', { outputFile: "report.json" }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
